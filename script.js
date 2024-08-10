79e3d53f4ee0457c10eda392f6b263947e2896fe
Sid-creates <siddhardach@outlook.com>
2024-08-09 20:00:33 -0400

    multiline and OOP

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+
 canvas.width = window.innerWidth * 0.9;
 canvas.height = window.innerHeight * 0.9;
 
-ctx.linewidth = 10;
-ctx.linecap = 'round';
-ctx.strokeStyle = 'blue';
 
 class Line{
-    constructor(){ //makes the instance of the class
+    constructor(canvas){ //makes the instance of the class
         //sets the properties the line
-        this.startX = Math.random() * canvas.width;
-        this.startY = Math.random() * canvas.width;
-        this.endX = Math.random() * canvas.width;
-        this.endY = Math.random() * canvas.width;
+        this.canvas = canvas;
+        this.startX = Math.random() * this.canvas.width;
+        this.startY = Math.random() * this.canvas.width;
+        //this.endX = Math.random() * this.canvas.width;
+        //this.endY = Math.random() * this.canvas.width;
+
+        this.history = [{x: this.x, y: this.y}];
+
+        this.lineWidth = Math.floor(Math.random() * 15 + 1);
+        this.hue = Math.floor(Math.random() * 360);
     }
-    draw(){ // draw action for the line
-        ctx.beginPath();
-        ctx.moveTo(this.startX, this.startY);
-        ctx.lineTo(this.endX, this.endY);
-        ctx.stroke();
+    draw(context){ // draw action for the line
+        //context.linecap = 'round';
+        context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
+        context.lineWidth = this.lineWidth;
+        context.beginPath();
+        context.moveTo(this.history[0].x, this.history[0].y);
+        for(let i = 0; i < 5; i++){
+            this.x = Math.random() * this.canvas.width;
+            this.y = Math.random() * this.canvas.width;
+            this.history.push({x: this.x, y: this.y});
+            }
+        for(let i = 0; i < this.history.length; i++){
+            context.lineTo(this.history[i].x, this.history[i].y);
+            //context.stroke();
+        }
+        context.lineTo(this.endX, this.endY);
+        context.stroke();
     }
 }
 
-const line1 = new Line(); //creates a new instance of the class
-line1.draw(); //draws the line
+//----------------------------------------------//
+
+const linesArray = [];
+const numberOfLines = 1;
+for(let i = 0; i < numberOfLines; i++){
+    linesArray.push(new Line(canvas));
+}
+
+console.log(linesArray);
+
+linesArray.forEach(object => {
+    object.draw(ctx);
+});
